53a3a7785f12a1671506aa3f01f7d74b2d333e55
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-06-30 22:17:40 -0400

    Initial commit

---
+++ src/main/java/org/george/moviecriticauthapi/MovieCriticAuthApiApplication.java
@@ -0,0 +1,12 @@
+package org.george.moviecriticauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MovieCriticAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCriticAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/george/moviecriticauthapi/MovieCriticAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.george.moviecriticauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCriticAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
